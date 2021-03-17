93805ecac2222ecd6ee291bb6144e79361514844
Caleb Jett <calebthejett@gmail.com>
2021-03-16 19:52:01 -0500

    auto-reformat code

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
-import spotipy, sys, os, json
-from spotipy.oauth2 import SpotifyClientCredentials, SpotifyOAuth
-from PyQt5 import QtWidgets as qtw, QtGui as qtg, QtCore as qtc, uic
 import urllib.request
 
+import json
+import os
 import pandas as pd
+import spotipy
+import sys
+from PyQt5 import QtWidgets as qtw, QtGui as qtg, QtCore as qtc, uic
+from spotipy.oauth2 import SpotifyOAuth
+
 
 def get_artist_names(artists):
     return [a['name'] for a in artists]
@@ -13,13 +17,14 @@ def update_track(library, track):
     if track['uri'] not in library:
         new_data = {
             'name': track['name'],
-            'tags':[],
-            'artists':get_artist_names(track['artists']),
+            'tags': [],
+            'artists': get_artist_names(track['artists']),
             'album_name': track['album']['name'],
             'album_art': track['album']['images'][0]['url'],
             'preview_url': track['preview_url']
         }
-        library.update({track['uri']:new_data})
+        library.update({track['uri']: new_data})
+
 
 class Spotibrarian:
     def __init__(self):
@@ -27,7 +32,8 @@ class Spotibrarian:
         scope = 'user-library-read playlist-modify-public'
         cid = "72e180d9f7a54590a1214f158cb264b5"
         cs = "20f168e6f0b94f098c495f6bca4df90d"
-        self._sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope, client_id = cid, client_secret = cs, redirect_uri="http://localhost:8888/callback"))
+        self._sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope, client_id=cid, client_secret=cs,
+                                                             redirect_uri="http://localhost:8888/callback"))
         self._init_library()
         self._ui.pbUpdate.clicked.connect(self.update_library)
         self._title = f"{self._sp.current_user()['display_name']}'s Spotify Library: "
@@ -46,7 +52,7 @@ class Spotibrarian:
         try:
             if len(self._library):
 
-                fname = qtw.QFileDialog.getSaveFileName(None, 'Save CSV',)[0]
+                fname = qtw.QFileDialog.getSaveFileName(None, 'Save CSV', )[0]
                 print(fname)
                 if len(fname):
                     try:
@@ -60,6 +66,7 @@ class Spotibrarian:
                         msg.exec_()
         except Exception as E:
             print(E)
+
     def _init_library(self):
         self._library = {}
         self._playlists = {}
@@ -75,12 +82,12 @@ class Spotibrarian:
         song_list = []
         results = self._sp.current_user_saved_tracks()
         for item in results['items']:
-            update_track(self._library,item['track'])
+            update_track(self._library, item['track'])
             song_list.append(item['track']['uri'])
         while results['next']:
             results = self._sp.next(results)
             for item in results['items']:
-                update_track(self._library,item['track'])
+                update_track(self._library, item['track'])
                 song_list.append(item['track']['uri'])
         poplist = []
         for k in self._library.keys():
@@ -90,9 +97,7 @@ class Spotibrarian:
             self._library.pop(k)
         self._update_library_view()
         self._update_tags()
-        self._ui.setWindowTitle(self._title+f" {len(self._library)} songs")
-
-
+        self._ui.setWindowTitle(self._title + f" {len(self._library)} songs")
 
     def _update_tags(self):
         self._tags = []
@@ -117,7 +122,7 @@ class Spotibrarian:
                                                        data['album_name'],
                                                        uri,
                                                        ', '.join(data['tags'])]))
-            #self._ui.update()
+            # self._ui.update()
         self._save_library()
         if row > -1:
             if row < self._ui.twSongs.topLevelItemCount():
@@ -125,11 +130,12 @@ class Spotibrarian:
                 self._ui.twSongs.setCurrentItem(self._ui.twSongs.topLevelItem(row))
                 self._ui.twSongs.scrollToItem(self._ui.twSongs.topLevelItem(row))
         self._library_update_flag = False
+
     def _save_library(self):
         dump_dict = {
-            'library':self._library,
-            'playlists':self._playlists,
-            'tags':self._tags
+            'library': self._library,
+            'playlists': self._playlists,
+            'tags': self._tags
         }
         with open('library.json', 'w') as f:
             json.dump(dump_dict, f)
@@ -139,7 +145,9 @@ class Spotibrarian:
             try:
                 if self._current_song is not None:
                     if self._current_song in self._library:
-                        self._library[self._current_song]['tags'] = sorted(list(set([tag.strip() for tag in str(self._ui.ptTags.toPlainText()).replace('\n','').replace('\t','').split(',') if tag.strip() != ''])))
+                        self._library[self._current_song]['tags'] = sorted(list(set([tag.strip() for tag in str(
+                            self._ui.ptTags.toPlainText()).replace('\n', '').replace('\t', '').split(',') if
+                                                                                     tag.strip() != ''])))
                         self._update_library_view()
                         self._update_tags()
                 if self._ui.twSongs.currentItem() is not None:
@@ -164,22 +172,18 @@ class Spotibrarian:
                     self._ui.lbSample.setText(f"Play Sample")
                     self._ui.ptTags.clear()
                     image = qtg.QImage()
-                    image.loadFromData(urllib.request.urlopen("https://www.solidbackgrounds.com/images/950x350/950x350-arsenic-solid-color-background.jpg").read())
+                    image.loadFromData(urllib.request.urlopen(
+                        "https://www.solidbackgrounds.com/images/950x350/950x350-arsenic-solid-color-background.jpg").read())
                     self._pixmap = qtg.QPixmap(image).scaled(128, 128, qtc.Qt.KeepAspectRatio)
                     self._ui.albumArt.setPixmap(self._pixmap)
 
             except Exception as E:
                 print(E)
             pass
-            #self._ui.lbArtist.
+            # self._ui.lbArtist.
+
 
 if __name__ == "__main__":
     app = qtw.QApplication(sys.argv)  # Create an instance of QtWidgets.QApplication
     window = Spotibrarian()  # Create an instance of our class
     app.exec_()  # Start the application
-
-# lib = {}
-# print(lib)
-# update_tracks(lib)
-# for k, v in lib.items():
-#     print(k, v)
